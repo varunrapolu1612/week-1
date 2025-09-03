6543a27f2adf7ddec8b3ab1793f77625e9504c3f
varunrapolu1612 <rvarun@iu.edu>
2025-09-03 18:30:35 -0400

    Update apputil.py

--- apputil.py
+++ apputil.py
@@ -1,21 +1,7 @@
-# Exercise-1:
 def palindrome(word):
     n = len(word)
     for i in range(n // 2):
         if word[i] != word[n - i - 1]:
             return False
     return True
-
-# Exercise -2:
-'''
-def Brackets(x):
-    count = 0
-    for char in x:
-        if char == '(':
-            count += 1
-        elif char == ')':
-            count -= 1
-        if count < 0:
-            return False #closing > opening
-    return count == 0  #Done!
-'''
+    
