e0f5080e4c758e7e0c6bd0e1156c3e82ad71b8ca
varunrapolu1612 <rvarun@iu.edu>
2025-09-03 18:47:32 -0400

    Update apputil.py

--- apputil.py
+++ apputil.py
@@ -1,8 +1,8 @@
 def palindrome(word):
+    word = word.replace(" ", "")
     word = word.lower()
     n = len(word)
     for i in range(n // 2):
         if word[i] != word[n - i - 1]:
             return False
     return True
-    
